bb03c3f15ed8833c23e8b8d87e1472d5e36178f7
mimiring <island_2@naver.com>
2022-12-07 00:21:43 +0900

    :hammer: express session을 서버에서 사용해보기

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -10,6 +11,21 @@ app.set("view engine", "ejs");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "message no one knows",
+    resave: true, // consoledp 나타나는 것
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
