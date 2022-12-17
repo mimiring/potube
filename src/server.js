b99ad916f21439a9ce8908bb11082af5c8d5e86b
mimiring <island_2@naver.com>
2022-12-18 01:31:35 +0900

    :hammer: 숨겨야 하는 내용을 환경변수를 사용해서 적용

--- src/server.js
+++ src/server.js
@@ -16,10 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "message no one knows",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/potube" }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
