f6eb0bff169ce6bfa9c81170ff7a3612e095255d
mimiring <island_2@naver.com>
2022-12-20 23:35:52 +0900

    :fire: 불필요코드 삭제

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Potube";
-  console.log(req.session);
-
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
 
