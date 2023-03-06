8a9db7a698af9c9dfacfc19fb7fa1ba31bd64fca
mimiring <island_2@naver.com>
2023-03-07 00:41:53 +0900

    :hammer: file upload router 추가

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,12 +7,17 @@ import {
   putEditProfile,
   putEditPassword,
 } from "../controllers/userController";
+import { uploadFiles } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.route("/github/start").get(startGithubLogin);
 userRouter.route("/github/finish").get(finishGithubLogin);
-userRouter.route("/:id").get(see).put(putEditProfile).delete(remove);
+userRouter
+  .route("/:id")
+  .get(see)
+  .put(uploadFiles.single("avatar"), putEditProfile)
+  .delete(remove);
 userRouter.route("/:id/editPassword").put(putEditPassword);
 
 export default userRouter;
